d1848e19eb073bb2a13718d2addc948d5b2284dd
cuncu <cuncu@DESKTOP-PQ5FM4I>
2021-12-14 16:32:58 +0100

    fixed header and nav

---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import FlagIcon from "@mui/icons-material/Flag";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import StoreFrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import SupervicedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import { Avatar, IconButton } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import ForumIcon from "@mui/icons-material/Forum";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img
+          src=" https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"
+          alt=""
+        />
+        <div className="header_input">
+          <SearchIcon />
+          <input type="text" placeholder="Search Wajibuug" />
+        </div>
+      </div>
+      <div className="header_center">
+        <div className="header_option header_option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StoreFrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervicedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+      <div className="header_right">
+        <div className="header_info">
+          <Avatar />
+          <h4>Raaage</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4AwZxySNzeX-V-cWLM5sPkWJcs_QGhxQ",
+  authDomain: "wb-clone-b4d2a.firebaseapp.com",
+  projectId: "wb-clone-b4d2a",
+  storageBucket: "wb-clone-b4d2a.appspot.com",
+  messagingSenderId: "382783260722",
+  appId: "1:382783260722:web:e2ba5179a77827d847d42b",
+  measurementId: "G-BZCW5063R4",
+};
